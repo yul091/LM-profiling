0587baa209ac9b655e738da81ec9fcf4694280bd
yul091 <yul091@ucsd.edu>
2023-11-12 17:09:58 -0800

    plot diff phases

--- plot.py
+++ plot.py
@@ -72,6 +72,71 @@ def plot(args, node: int = None):
     idle_f = f'{output_dir}/idle_time_{execution}_{setting}_node{node}.json' if node is not None else f'{output_dir}/idle_time_{execution}_{setting}.json'
     with open(idle_f, 'w') as f:
         json.dump(idle_dict, f)
+        
+        
+
+def plot_mix(args, node: int = None):
+    output_dir = args.output_dir
+    coroutine = args.coroutine
+    setting = args.setting
+    
+    # Load timing information
+    execution = 'coroutine' if coroutine else 'sync'
+    stats_f = f'{output_dir}/timing_info_{execution}_{setting}_node{node}.json' if node is not None else f'{output_dir}/timing_info_{execution}_{setting}.json'
+    with open(stats_f, 'r') as f:
+        timing_info = json.load(f)
+
+    # Normalize the times by the smallest timestamp
+    # min_time = min(min(times[0] for times in timing_info.values() if times), default=0)
+    min_time = None
+    for times_list in timing_info.values():
+        for times in times_list:
+            if min_time is None or times[0] < min_time:
+                min_time = times[0]
+    min_time = min_time or 0
+    timing_info = {k: [[t[0] - min_time, t[1]] for t in v] for k, v in timing_info.items()}
+
+    # Extract the number of GPUs based on the keys
+    gpus = list(set(int(key.split('_')[0]) for key in timing_info))  # GPUs are 0-indexed
+    init_gpu, last_gpu = min(gpus, default=0), max(gpus, default=0)
+    num_gpus = len(gpus)
+    # Create a figure and axis to plot on
+    fig, ax = plt.subplots(figsize=(20, num_gpus))
+
+    # Define colors for each operation
+    colors = {'forward': 'b', 'forward_loss': 'g', 'backward': 'r'}
+
+    # Plot the timings for each GPU
+    for gpu_id in range(len(gpus)):
+        start_times = timing_info.get(f"{gpu_id+init_gpu}_start", [])
+        end_times = timing_info.get(f"{gpu_id+init_gpu}_end", [])
+
+        # Plot each task for this GPU, increase the linewidth for a wider bar
+        for (start, start_label), (end, end_label) in zip(start_times, end_times):
+            color = colors.get(start_label, 'k')  # default color is black if label not found
+            ax.hlines(y=gpu_id + 1, xmin=start, xmax=end, colors=color, linewidth=20, label=start_label)
+
+    # Set plot labels and grid
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('GPU')
+    ax.set_title('Task Inference Profiling')
+    ax.set_yticks(range(1, len(gpus) + 1))
+    ax.set_yticklabels([f'GPU {i}' for i in range(len(gpus))])
+    ax.grid(True)
+
+    # Reverse the y-axis so that GPU 0 is at the top
+    ax.invert_yaxis()
+
+    # Create a custom legend
+    handles, labels = ax.get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    ax.legend(by_label.values(), by_label.keys())
+
+    # Show the plot
+    plt.tight_layout()
+    plt.savefig(f'{stats_f.split(".")[0]}.png')
+    plt.show()
+
 
 
 if __name__ == '__main__':
@@ -83,7 +148,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     if not args.node:
-        plot(args)
+        # plot(args)
+        plot_mix(args)
     else:
         for node in range(args.node):
-            plot(args, node)
+            # plot(args, node)
+            plot_mix(args, node)
