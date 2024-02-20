5fc56154928750826dd92dabb4ad9eaf41e9eec1
yul091 <yul091@ucsd.edu>
2024-02-20 13:11:38 -0800

    medium script calibration

--- scripts/distributed_dialogpt-medium_active_poisson.sh
+++ scripts/distributed_dialogpt-medium_active_poisson.sh
@@ -5,27 +5,30 @@ WORKLOAD=poisson
 SETTING=active
 OUTPUT_DIR=prof_async
 MODEL_NAME=dialogpt-medium
-RATE_LAMBDA=60 # number of tasks arriving per second
 
-# 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
-for RETRAIN_RATE in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-    CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python distributed_dialogpt.py \
-        --model_name_or_path "microsoft/DialoGPT-medium" \
-        --model_name $MODEL_NAME \
-        --num_nodes $NUM_NODES \
-        --n_samples $NUM_SAMPLES \
-        --workload $WORKLOAD \
-        --rate_lambda $RATE_LAMBDA \
-        --setting $SETTING \
-        --retraining_rate $RETRAIN_RATE \
-        --output_dir $OUTPUT_DIR \
-        --batch_size $BATCH_SIZE
+for RATE_LAMBDA in 10 20 30 40 50 60; do
+    OUTPUT_DIR=prof/lambda_${RATE_LAMBDA}/$MODEL_NAME
 
-    python plot.py \
-        --node $NUM_NODES \
-        --model_name $MODEL_NAME \
-        --setting $SETTING \
-        --workload $WORKLOAD \
-        --retraining_rate $RETRAIN_RATE \
-        --output_dir $OUTPUT_DIR
+    # 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+    for RETRAIN_RATE in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
+        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python distributed_dialogpt.py \
+            --model_name_or_path "microsoft/DialoGPT-medium" \
+            --model_name $MODEL_NAME \
+            --num_nodes $NUM_NODES \
+            --n_samples $NUM_SAMPLES \
+            --rate_lambda $RATE_LAMBDA \
+            --workload $WORKLOAD \
+            --setting $SETTING \
+            --retraining_rate $RETRAIN_RATE \
+            --output_dir $OUTPUT_DIR \
+            --batch_size $BATCH_SIZE
+
+        python plot.py \
+            --node $NUM_NODES \
+            --model_name $MODEL_NAME \
+            --setting $SETTING \
+            --workload $WORKLOAD \
+            --retraining_rate $RETRAIN_RATE \
+            --output_dir $OUTPUT_DIR
+    done
 done
--- scripts/distributed_dialogpt-medium_isolated_poisson.sh
+++ scripts/distributed_dialogpt-medium_isolated_poisson.sh
@@ -3,14 +3,14 @@ NUM_SAMPLES=1000
 BATCH_SIZE=3
 WORKLOAD=poisson
 SETTING=isolated
-MODEL_NAME=dialogpt-small
+MODEL_NAME=dialogpt-medium
 RATE_LAMBDA=10 # number of tasks arriving per second
 OUTPUT_DIR=prof/lambda_${RATE_LAMBDA}/$MODEL_NAME
 
 # 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
-for RETRAIN_RATE in 0.8 0.9 1.0; do
+for RETRAIN_RATE in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
     CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python distributed_dialogpt.py \
-        --model_name_or_path "microsoft/DialoGPT-small" \
+        --model_name_or_path "microsoft/DialoGPT-medium" \
         --model_name $MODEL_NAME \
         --num_nodes $NUM_NODES \
         --n_samples $NUM_SAMPLES \
